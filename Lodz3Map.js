502bf9617f28f62050e529d7880efbb86664bdd6
SparklesReal <jultomtenriktig@gmail.com>
2026-07-30 06:05:47 +0200

    Lodz 3 start

---
+++ Lodz3Map.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([51.75964289598423, 19.46316965563376], 9);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
